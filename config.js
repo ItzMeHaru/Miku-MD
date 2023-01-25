d2635ddfb040c7f71f460cd96a05cb751ed08fbe
ItzMeHaru <123491050+ItzMeHaru@users.noreply.github.com>
2023-01-25 23:21:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ global.OwnerNumber = ['916282984554']  // -- Add multiple numbers like  ['918101
 global.ownertag = ['916282984554']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
 global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.author = "By ItzMeHaru"
+global.OwnerName = "Albin"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['.'] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "Kerala, India"
